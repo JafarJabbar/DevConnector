92bd56511e3a9970fb678a71803ca3d3dd66e78e
Jafar <jafar@zeroline.az>
2021-03-23 16:07:46 +0400

    Initial commit

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express=require('express');
+const connectDB = require('./config/db');
 const app=express();
-
+const db=require('./config/db');
 const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
+app.use('/api/auth',require('./routes/api/auth'));
 
 app.get('/',(req,res)=>{
     res.send('API running')
